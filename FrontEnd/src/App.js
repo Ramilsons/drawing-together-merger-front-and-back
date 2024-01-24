c9dc8c05d72020fcc460e28bca989157cf08ce2f
Ramilsons <ramilsonfelixsilva9@gmail.com>
2024-01-24 00:02:24 -0300

    feat(chat): add image above chat

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -9,6 +9,7 @@ import SendResponse from "./components/SendResponse";
 import Historic from "./components/Historic";
 
 import trophyImage from "./images/trophy.webp";
+import microphoneImage from "./images/microphone.webp";
 
 function App() {
     const [isInitializePossible, setIsInitializePossible] = useState(false);
@@ -53,16 +54,18 @@ function App() {
                 <HeaderInfos isMyTurn={isMyTurn} wordGenerated={wordGenerated} timer={timer} />
 
                 <div className={'flex items-end justify-between'}>
-                    <div className={`relative ${isMyTurn ? '' : 'pointer-events-none'}`}>
+                    <div className={`relative`}>
                         <div className={`border-[#fff] border-4 rotate-[-3deg] w-[750px]`}>
-                            <div className={`rotate-[3deg]`}>
+                            <div className={`rotate-[3deg] ${isMyTurn ? '' : 'pointer-events-none cursor-not-allowed'}`}>
                                 <ReactP5Wrapper sketch={sketch} />
                             </div>
                         </div>
                         <img className="absolute bottom-[-50px] left-[-80px]" width="140px" height="140px" alt="Troféu" src={trophyImage} /> 
                     </div>
 
-                    <div className={`border-[#fff] border-4 rotate-3 w-[330px   ]`}>
+                    <div className={`border-[#fff] border-4 rotate-3 w-[330px]`}>
+                        <img className="absolute top-[-80px] right-[-20px] z-10" width={140} height={140} alt="Megafone" src={microphoneImage} />
+
                         <div className={`rotate-[-3deg] bg-[#fff] h-[500px] w-[330px]`}>
                             <div className={'py-5 px-4'}>
                                 <Historic correctResponse={wordGenerated} isMyTurn={isMyTurn} />
--- FrontEnd/src/components/SendResponse.js
+++ FrontEnd/src/components/SendResponse.js
@@ -24,7 +24,7 @@ export default function SendResponse(props) {
         <div className={'absolute bottom-0 h-[60px] w-[100%] border-t-2 p-3 flex items-center justify-between'}>
             <input className={'border-b-2 w-[250px]  h-[40px] focus:outline-none'} placeholder="Escreva aqui..." type="text" onChange={(e) => { newResponse(e) }}></input>
             <button className={'border-2 rounded-xl w-[50px] h-[40px] flex items-center justify-center'} type="button" onClick={verifyResponse} aria-label="Enviar palavra">
-                <img src={sendIcon} alt="Avião de papel" width={35} height={35} />
+                <img src={sendIcon} loading="lazy" alt="Avião de papel" width={35} height={35} />
             </button>
         </div>
     );
