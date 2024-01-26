d75ecd0c800aa1fbb7d846faee31ebe0c16d810b
Ramilsons <ramilsonfelixsilva9@gmail.com>
2024-01-26 19:52:03 -0300

    fix(changeTurn): fix change turn after correct aswer

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require("cors");
-const { userConnected, userDisconnected, myTurnFinished, saveResponseOnHistoric, managerTimeBreak } = require('./utils/usersLogic');
+const { userConnected, userDisconnected, myTurnDrawFinished, saveResponseOnHistoric, managerTimeBreak } = require('./utils/usersLogic');
 
 const app = express();
 app.use(cors());
@@ -21,7 +21,7 @@ io.sockets.on('connection', (socket) => {
     userConnected(io, socket);
 
     // Listening client data
-    socket.on('myTurnFinished', () => { myTurnFinished(socket, io) });
+    socket.on('myTurnDrawFinished', () => { myTurnDrawFinished(socket, io) });
     socket.on('answer', (data) => { saveResponseOnHistoric(data, io) });
     socket.on('disconnect', () => { userDisconnected(socket, io) });
     socket.on('timeBreak', () => { managerTimeBreak(socket, io) });
--- BackEnd/utils/usersLogic.js
+++ BackEnd/utils/usersLogic.js
@@ -23,7 +23,7 @@ function verifyLimit(io, socket) {
     if(usersConnected == 2) {
         // Sending data to client
         io.emit('userNumberChanged', { isPossibleInit: true, usersConnected: usersConnected });
-        if(isSendFirstTurn == false) {myTurnFinished(socket, io)};
+        if(isSendFirstTurn == false) {myTurnDrawFinished(socket, io)};
         isSendFirstTurn = true
     }else {
        // socket.broadcast.emit('userNumberChanged', { isPossibleInit: false, usersConnected: usersConnected });
@@ -35,7 +35,9 @@ function verifyLimit(io, socket) {
 
 let intervalController;
 
-function myTurnFinished(socket, io) {
+// Run when he has finished drawing
+function myTurnDrawFinished(socket, io) {
+    console.log('myTurnDrawFinished');
     let timer = 60;
     let wordToDraw = '';
     let controller = false;
@@ -61,7 +63,13 @@ function myTurnFinished(socket, io) {
         }
     }
     
+    // your turn to draw
     socket.broadcast.emit('yourTurn'); 
+
+    let idIsNotDrawing = socket.id;
+
+    // send to me "is not my turn to draw"
+    io.to(idIsNotDrawing).emit('finishDrawingTurn');
 }
 
 function saveResponseOnHistoric(data, io) {
@@ -70,14 +78,54 @@ function saveResponseOnHistoric(data, io) {
     io.emit('newResponseOnHistoric', wordsHistoric);
 }
 
+// Run when user send correct answer and rival draw is finished
+function rivalDrawFinished(socket, io) {  
+    myAnswerTurnFinished(socket, io);
+}
+
+function myAnswerTurnFinished(socket, io) {
+    let timer = 60;
+    let wordToDraw = '';
+    let controller = false;
+
+    while(controller != true) {
+
+        let randomNumber = Math.floor(Math.random() * words.length);
+        let randomWord = words[randomNumber];
+        
+
+        if(wordsHistoric.indexOf(randomWord) == -1) {
+            wordToDraw = randomWord;
+
+            io.emit('wordGenerated', wordToDraw);
+
+            intervalController = setInterval(() => {
+                if(timer > 0) {
+                    timer = timer - 1;
+                    io.emit('timerController', timer);
+                }
+            }, 1000)
+            controller = true;
+        }
+    }
+    
+    let idIsNotDrawing = socket.id;
+
+    // my turn to draw
+    io.to(idIsNotDrawing).emit('yourTurn');
+
+    // send to rival "is not your turn to draw"
+    socket.broadcast.emit('finishDrawingTurn');
+}
+
 function managerTimeBreak(socket, io) {
     io.emit('break');
 
     setTimeout(() => {
         io.emit('finishBreak');
         clearInterval(intervalController);
-        myTurnFinished(socket, io);
+        rivalDrawFinished(socket, io);
     }, 10000);
 }
 
-module.exports = {userConnected, userDisconnected, myTurnFinished, saveResponseOnHistoric, managerTimeBreak};
+module.exports = {userConnected, userDisconnected, myTurnDrawFinished, saveResponseOnHistoric, managerTimeBreak};
